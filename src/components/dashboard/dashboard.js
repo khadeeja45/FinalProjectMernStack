3f53bb5b8cf4389b365a1763d8d2b87d974685b4
atiya-331 <khanatiya331@gmail.com>
2018-06-06 03:50:57 -0700

    initiate-commit

---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SecondSection from "./main/second_section_dashboard";
+import WebsiteOVerView from "./main/website_overview";
+export default () => {
+    return (
+        <div>
+             <WebsiteOVerView/>
+             <SecondSection/>
+        </div>   
+    )
+}
